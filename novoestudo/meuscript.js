bca05683397f13a1abe37ef4aeeab1d9f7fec604
Gabriel <107438394+gabrielfinazzo@users.noreply.github.com>
2023-03-10 01:16:53 -0300

    Atualizar estudo javascript

--- novoestudo/meuscript.js
+++ novoestudo/meuscript.js
@@ -1,2 +1,15 @@
-alert('Olá, seja bem vindo ao curso!')
-document.getElementById('nome').value = 'Oi'
+//string
+var texto = 'Curso de JavaScript'
+
+//number
+var numeroInteiro = -7 //int
+var numeroFracionado = 10.75 //float
+
+//boolean
+var teste = true
+var teste2 = false
+
+//alert(numeroFracionado) //abrir um dialogo
+//document.write('Olá')
+
+console.log(numeroInteiro)
