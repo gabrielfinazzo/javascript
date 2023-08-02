d2673bc0bedd3b0c0898d9acd7471d31255a78a1
Gabriel <107438394+gabrielfinazzo@users.noreply.github.com>
2023-08-02 01:46:02 -0300

    Atualização estudos javascript

--- Hora de Codar/fundamentos.js
+++ Hora de Codar/fundamentos.js
@@ -1,1 +1,245 @@
 // Fundamentos
+
+// var nome = "valor"
+var minhaVariavel = "Olá, mundo!";
+
+console.log(minhaVariavel);
+
+
+
+// variáveis e tipos de dados
+var meuNumero = 10;
+console.log(meuNumero + 5);
+
+console.log(typeof meuNumero); // typeof serve para identificar o tipo do dado. Nesse caso o dado é numérico.
+
+var booleano = true; // false
+
+console.log(booleano);
+
+var meuObjeto = {};
+var meuArray = [];
+var meuNull = null;
+var meuUndefined = undefined;
+
+console.log(meuObjeto);
+console.log(meuArray);
+console.log(meuNull);
+console.log(meuUndefined);
+
+
+// let e cont
+// novas formas de declarar variáveis
+
+let x = 10; // substitui o var
+const y = 5; // variável para declarar uma constante, valor que não muda ao longo do código.
+
+console.log(typeof x)
+console.log(x, y);
+
+
+// Operadores aritméticos
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+
+// Operadores de comparação
+console.log(x == y); // igual
+console.log(x != y); // diferente
+
+console.log("5" == 5)
+console.log("5" === 5) // caracteres de tipos iguais. Nesse caso é falso, pois 5 string é diferente de 5 numérico.
+console.log("5" !== 5) // caracteres de tipos diferentes. Nesse caso é verdadeiro.
+
+
+// Operadores lógicos
+// AND &&
+// OR ||
+
+console.log(10 > 5 && 20 > 5); // 10 é maior que 5 e 20 é maior que 5. Verdadeiro!
+console.log(10 > 5 && 20 < 5); // 10 é maior que 5 e 20 é menor que 5. Falso! 20 não é menor que 5.
+
+console.log(10 > 5 || 20 < 5); // Se um dos dois for verdadeiro o resultado será true. Nesse caso o 10 > 5 é verdadeiro. 
+
+
+// Conversão de tipos
+const meuNumero2 = "123";
+const meuNumeroConvertido = Number(meuNumero2);
+console.log(meuNumeroConvertido)
+console.log(typeof meuNumeroConvertido)
+
+
+// Estrutura de condição - if, else, else if
+
+const idade = 19;
+
+if (idade <= 13) {
+    console.log("Criança");
+} else if (idade <= 18) {
+    console.log("Adolescente")
+} else {
+    console.log("Adulto");
+}
+
+if (false) {
+    console.log("Isso exxecuta");
+} else {
+    console.log("Isso agora é executado!");
+}
+
+
+// Switch
+const fruta = "Banana"
+
+switch(fruta) {
+    case "Banana":
+        console.log("Banana é a fruta!");
+        break
+    case "Maçã":
+        console.log("Maçã é a fruta!");
+        break
+    default:
+        console.log("Fruta não encontrada");
+}
+
+
+
+// Estruturas de repetição
+//1, 2, 3, 4, 5, 6... => dependendo de uma condição
+
+// contador, condição de limite, incremento
+for(let i = 0; i < 5; i = i + 1) {
+    console.log("O valor de i é: " + i);
+}
+//primeiro vem a declaração da variável; em sequÊncia até quando haverá repetições ( i < 5); e por final o valor de i (i = i + 1).
+//Em vez de usar i = i + 1, pode ser utilizado i++
+
+// while
+let k = 0;
+
+while(k < 5) {
+    console.log("O valor de k é: " + k)
+    k++;
+}
+
+// do while
+let j = 0;
+
+do {
+    console.log("O valor do j é: " + j);
+    j++
+} while(j < 5);
+
+
+
+// Funções
+// function nome(arg1, arg2) { corpo }
+function cumprimentar(nome) {
+    console.log("Olá!" + nome);
+}
+
+cumprimentar("Matheus");// Tem que chamar a função para que o código seja ativado.
+
+
+// Escopo de variáveis
+let cor = "azul";
+
+function mostrarCor() {
+    let cor = "Verde";
+    console.log(cor);
+}
+
+console.log(cor); // Escopo global, esta fora da função
+
+mostrarCor(); // Escopo particular, esta presente somente na função
+
+
+// hoisting = içamento
+testeHoisting(); // É possível usar a função mesmo antes da declaração da mesma, isso ocorre por conta do sistema de içamento (hoisting).
+
+function testeHoisting() {
+    console.log("Deu certo!")
+}
+
+
+// arrow function
+const testeArrow = () => console.log("Isso também é uma função.")
+testeArrow();
+
+
+// Truthy e falsy
+const minhaVariavel1 = "" // falsy
+const minhaVariavel2 = "Algum texto" // verdadeiro
+
+if (minhaVariavel1) {
+    console.log("É verdadeiro!");
+} else {
+    console.log("É falso!");
+}
+
+if (minhaVariavel2) {
+    console.log("É verdadeiro!");
+} else {
+    console.log("É falso!");
+}
+
+// A variável1 é falsa, pois é uma string sem caracter. Já a variável2 é verdadeira, pois é uma string com caracters.
+
+
+
+// Array - listas
+const numeros = [1, 2, 3, 4, 5];
+console.log(numeros);
+
+console.log(numeros[0]);
+
+console.log(numeros[2]);
+
+numeros.push(6); // Adiciona mais um número ao array
+
+console.log(numeros);
+
+
+// prototype => Objeto -> objeto2
+// Array => nossos arrays
+numeros.pop() // Remove o último número do array
+
+console.log(numeros);
+
+
+// strings
+
+const minhaStringNova = "Olá, mundo!"
+
+// concatenação
+const minhaString3 = minhaStringNova + " Como você está?";
+
+console.log(minhaString3);
+
+// interpolação
+const minhaString4 = ` ${minhaStringNova}Como você está?`;
+
+console.log(minhaString4);
+
+console.log(minhaString4.length); // qtd de caracteres
+console.log(minhaString4[2]); // trazer o caracter escolhido
+console.log(minhaString4.toLocaleUpperCase());
+
+
+// Data e hora
+const agora = Date();
+console.log(agora);
+
+const natal = new Date(2023, 11, 25);
+console.log(natal);
+
+
+// Math
+console.log(Math.PI);
+
+console.log(Math.round(3.4)); // Arrendondar
+
+console.log(Math.sqrt(16)); // Raiz quadrada
+
+console.log(Math.pow(2, 4)); // potência 2 * 2 * 2 * 2 = 16
---
+++ Hora de Codar/fundamentos_exercicios.js
@@ -0,0 +1,57 @@
+// Exercícios de fundamentos
+// Exercício 1: Olá, Mundo!
+//Crie um script que imprima "Olá, Mundo!" no console
+
+console.log("Olá, Mundo!");
+
+
+// Exercício 2: Conversão de tipos
+// Dado o valor de uma string "1234", converta-o em um número e exiba o tipo da variável no console.
+
+const stringConverter = "1234";
+const number = Number(stringConverter);
+console.log(number);
+console.log(typeof number);
+
+
+// Exercício 3: Manipulação de strings
+// Dado uma string "JavaScript é incrível", escreva um código que conte quantos caracteres a string tem e quantas palavras existem na frase.
+
+const newString = "JavaScript é incrível".toLowerCase()
+console.log(newString)
+const newStringWords = newString.split(" "); // Split separa cada caracter em um arrey
+console.log("Ao todo são " + newString.length + " caracteres e " + newStringWords.length + " palavras presentes nessa string.")
+
+
+// Exercício 4: Loops e Arrays
+// Crie um array com cinco nomes. Use um loop for para imprimir cada nome no console.
+
+const nomes = ["Gabriel", "João", "Maria", "Clara", "Rodolfo"]
+const nomesQuantidade = nomes.length
+
+for (let i = 0; i < nomesQuantidade; i++) {
+    console.log(nomes[i])
+}
+
+
+// Exercício 5: Funções, Strings e Math
+//Crie uma função que aceita uma string representando um horário no formato de 24 horas (por exemplo, "14:30"). A função deve retornar uma string que converta o horário para o formato de 12 horas (por exemplo, "2:30PM").
+// Use o objeto Math para auxiliar nesta conversão. Certifique-se de que sua função lida corretamente com horários na meia-noite e no meio-dia.
+
+/* const hora = "18:30";
+const horaQuant = hora.split(":");
+const horaNum = Number(horaQuant[0] + "." + horaQuant[1]);
+ */
+function converterHora (horario24) {
+    const [hora, minuto] = horario24.split(":");
+    const hora12 = hora % 12 || 12;
+    let periodo = "AM";
+
+    if (hora > 12) {
+        periodo = "PM"
+    }
+    
+    console.log(`${hora12}:${minuto} ${periodo}`)
+}
+
+converterHora("24:16")
