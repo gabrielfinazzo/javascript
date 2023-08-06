6a0079df21ed7271fbde8a438bc7fc455da70026
Gabriel <107438394+gabrielfinazzo@users.noreply.github.com>
2023-08-06 02:15:05 -0300

    Atualização DOM

--- Hora de Codar/dom.js
+++ Hora de Codar/dom.js
@@ -73,3 +73,71 @@ console.log(primeiroFilho);
 
 const ultimoFilho = pai.lastChild;
 console.log(ultimoFilho);
+
+
+// Manipulação de estrutura do DOM
+const novoElemento = document.createElement("div");
+console.log(novoElemento);
+
+novoElemento.textContent = "Minha div de JavaScript";
+console.log(novoElemento);
+
+document.body.appendChild(novoElemento);
+document.body.insertBefore(novoElemento, pai); // appendChil e insertBefore serve para adicionar o elemento na página HTML, para que aparece no site e na posição desejada.
+
+document.body.removeChild(elementoPorId); // Remove o elemento desejado da página html.
+
+
+// Eventos
+
+// click
+const botao = document.querySelector("button");
+
+botao.addEventListener("click", function () {
+    console.log("Botão clicado!");
+});
+
+// mouse
+const elemento5 = document.querySelector("#meuFormulario");
+
+elemento5.addEventListener("mouseover", function() {
+    console.log("O mouse passou aqui!");
+});
+
+// teclado
+const campoInput = document.querySelector("#meuInput");
+campoInput.addEventListener("keydown", function() {
+    console.log("Tecla pressionada");
+});
+
+// formulario
+const form = document.querySelector("form");
+form.addEventListener("submit", function(event) { 
+    event.preventDefault(); // Event serve para previnir que a página não recarregue ao acionar o submit (enviar).
+    console.log("Form enviado");
+});
+
+
+// Propagação de eventos
+document.querySelector("#elementoPai").addEventListener("click", function() {
+    console.log("Click capturado no pai");
+}); // O evento ocorre se os elementos filhos forem clicados também. Tomar cuidado para usar somente nos elementos filhos para não acarretar em bug.
+
+document.querySelector("#elementoFilho").addEventListener("click", function(event) {
+    //event.stopPropagation(); // Esse evento faz com que o evento click atribuido ao elemento pai não afete mais o elemento filho.
+
+    console.log("Elemento filho");
+});
+
+document.querySelector("#meuLink").addEventListener("click", function(event) {
+    event.preventDefault(); // Remove o comportamento padrão do elemento que seria redirecionar para o site do google
+
+    console.log("clicou no link!");
+});
+
+// delegação de eventos
+document.querySelector("#elementoPai").addEventListener("click", function(event) {
+    if (event.target.matches(".classeDosFilhos")) {
+        console.log("Evento delegado para o filho");
+    }
+}); // funciona o evento do pai mais o evento do filho ao clicar no elemento filho
