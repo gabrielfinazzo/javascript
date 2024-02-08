106e5a6cee6a4faba59d79eaf8f5c03f2b7faefd
Gabriel <107438394+gabrielfinazzo@users.noreply.github.com>
2024-02-08 20:49:18 -0300

    Estudo javascript

--- FullCourseForFree/index.js
+++ FullCourseForFree/index.js
@@ -1,26 +1,9 @@
+//condition ? codeIfTrue : codeIfFalse;
 
-const myCheckBox = document.getElementById('myCheckBox');
-const visaBtn = document.getElementById('visaBtn');
-const MasterCardBtn = document.getElementById('masterCardBtn');
-const payPalBtn = document.getElementById('payPalBtn');
-const mySubmit = document.getElementById('mySubmit');
-const subResult = document.getElementById('subResult');
-const paymentResult = document.getElementById('paymentResult');
+const email = "gabrielfinazzo@gmail.com";
 
-mySubmit.onclick = function() {
-    if(myCheckBox.checked) {
-        subResult.textContent = `You are subscribed!`;
-    } else {
-        subResult.textContent = `You are NOT subscribed!`; 
-    }
+let username = email.slice(0, email.indexOf("@"));
+let extension = email.slice(email.indexOf("@") + 1);
+console.log(username);
+console.log(extension);
 
-    if(visaBtn.checked) {
-        paymentResult.textContent = `You are paying with Visa`;
-    } else if(MasterCardBtn.checked) {
-        paymentResult.textContent = `You are paying with Master Card`
-    } else if(payPalBtn.checked) {
-        paymentResult.textContent = `You are paying with Pay Pal`
-    } else {
-        paymentResult.textContent = `You must select a payment type`
-    }
-}
